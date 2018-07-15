d20c018d56aa3d5e7b6e5b24ab496916aa4d5cd0
Alexander-Cox <alexander.b.cox@outlook.com>
2018-07-15 21:23:10 +0100

    connect up initial routing paths

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
+const favourites = require('./favourites.route');
+const inventory = require('./inventory.routes');
+const solutions = require('./solutions.routes');
+const users = require('./users.routes');
+
+router.use('/favourites', favourites);
+router.use('/inventory', inventory);
+router.use('/solutions', solutions);
+router.use('/users', users);
 
 module.exports = router;

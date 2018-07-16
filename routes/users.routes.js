587139dfa34f03d196bd6a91c1304ce5137edcce
Alexander-Cox <alexander.b.cox@outlook.com>
2018-07-16 11:58:04 +0100

    simplify users routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,20 +1,9 @@
 const router = require('express').Router();
 const controller = require('../controller/users.controller');
 
-router.get('/', (req, res) => {
-    return controller.getAllUsers(req, res);
-});
-
-router.get('/:username', (req, res) => {
-    return controller.getUserByUsername(req, res);
-});
-
-router.get('/:username/favourites', (req, res) => {
-    return controller.getFavouritesByUsername(req, res);
-});
-
-router.post('/', (req, res) => {
-    return controller.postNewUser(req, res);
-});
+router.get('/', controller.getAllUsers);
+router.get('/:username', controller.getUserByUsername);
+router.get('/:username/favourites', controller.getFavouritesByUsername);
+router.post('/', controller.postNewUser);
 
 module.exports = router;

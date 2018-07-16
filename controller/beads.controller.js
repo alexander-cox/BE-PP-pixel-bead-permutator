5f1f0d966d65fb4090028ee503ed3e99111ee0bc
Alexander-Cox <alexander.b.cox@outlook.com>
2018-07-16 15:17:21 +0100

    set up basic beads controller functions to test routes

--- controller/beads.controller.js
+++ controller/beads.controller.js
@@ -1,3 +1,9 @@
 module.exports = {
-    
+    getAllAvailableBeads(req, res) {
+        return res.status(200).send('get all beads');
+    },
+    postBeadsBySolutionID(req, res) {
+        const { solution_id } = req.params;
+        return res.status(201).send({ Beads: { ...req.body, solution_id } });
+    }
 }
--- routes/beads.routes.js
+++ routes/beads.routes.js
@@ -1,5 +1,8 @@
 router = require('express').Router();
 controller = require('../controller/beads.controller');
 
+router.get('/', controller.getAllAvailableBeads)
+
+router.post('/:solution_id', controller.postBeadsBySolutionID)
 
 module.exports = router;

724bfea69cba9fb5eb71d46f5e5848a45ea0a339
Alexander-Cox <alexander.b.cox@outlook.com>
2018-07-16 11:32:59 +0100

    make basic inventory connections

--- routes/inventory.routes.js
+++ routes/inventory.routes.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    return res.status(200).send('You made it to the inventory!!');
+    return res.status(200).send('Get All inventory list!!');
 });
+
+router.get('/:user_id', (req, res) => {
+    return res.status(200).send(`Get inventory of ${req.params.user_id}`)
+});
+
+router.put('/:inv_id', (req, res) => {
+    //use increment=true/false and amount=(int) on query string 
+    const { increment, amount } = req.query;
+    const { inv_id } = req.params;
+
+    //TEMPORARY to test the routed method works...
+    let strMove; 
+    increment === 'true' ? strMove='increment' : strMove='decrement';
+    return res.status(200).send(`${strMove} inventory number ${inv_id} by ${amount}`);
+});
+
+router.post()
 
 module.exports = router;

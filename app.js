e79e694f5dcbbfc7849d21eef2a81035c299f881
Alexander-Cox <alexander.b.cox@outlook.com>
2018-07-16 20:45:34 +0100

    resolve post request entity too large issue by increasing bodyparser limit

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '5mb'}));
 app.use('/api', apiRouter);
 app.get('/', (req, res) => {
     return res.status(200).send('You made it!!');
-})
+});
 
 module.exports = app;

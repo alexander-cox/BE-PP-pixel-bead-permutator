bcc3dee32a27196cc9d087458458be9b0c20fa73
Alexander-Cox <alexander.b.cox@outlook.com>
2018-07-26 14:46:43 +0100

    correction on db connection

--- db.js
+++ db.js
@@ -1,5 +1,11 @@
 const pgp = require('pg-promise')({promiseLib: Promise});
-const config = require(`./config/${process.env.NODE_ENV}.config.js`);
-console.log(`connected to ${process.env.NODE_ENV} db`);
+if (process.env.NODE_ENV === 'production') {
+    console.log(`connected to ${process.env.NODE_ENV} db`);
+    module.exports = pgp(process.env.DATABASE_URL);
+} else {
+    const config = require(`./config/${process.env.NODE_ENV}.config.js`);
+    console.log(`connected to ${process.env.NODE_ENV} db`);
+    module.exports = pgp(config.connection);
+}
 
-module.exports = pgp(process.env.DATABASE_URL || config.connection);
+
